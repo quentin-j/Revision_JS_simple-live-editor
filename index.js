cd0bd2ee5d02838419813910647a2f267de398e2
quentin-j <quentin678jougla@gmail.com>
2021-08-02 15:42:57 +0200

    stockage des éléments html

---
+++ index.js
@@ -0,0 +1,6 @@
+// On stocke dans des variables les éléments HTML sur lesquels on veut agir
+let inputText = document.querySelector('#input-text');
+let contenuHTML = document.querySelector('#contenu-html');
+
+
+
